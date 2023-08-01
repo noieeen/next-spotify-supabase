74f4628b8064570f6da736527f86433e93100ee6
noieeen <tharnnn1401@gmail.com>
2023-08-01 23:45:21 +0700

    like function

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,48 +1,42 @@
-'use client';
+"use client";
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
-
 interface SearchContentProps {
-    songs: Song[]
+  songs: Song[];
 }
 
 const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  const handleOnClick = () => {};
 
-    const handleOnClick = () => {
-
-    }
-
-    if (songs.length == 0) {
-        return (
-            <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-                No songs found.
-            </div>
-        )
-    }
-
+  if (songs.length == 0) {
     return (
-        <div className="flex flex-col gap-y-2 w-full px-6">
-            {songs.map((song) => (
-                // <div key={song.id}
-                //     className="flex items-center gap-x-4 w-full">
-                //     <div className="flex-1">
-                //         <MediaItem onClick={() => { }} data={song} />
-                //     </div>
-                // </div>
-                <div key={song.id} className="flex items-center gap-x-4 w-full">
-                    <div className="flex-1">
-                        <MediaItem onClick={() => { }}
-                            key={song.id}
-                            data={song} />
-                    </div>
-                    {/** Add Like Button Here */}
-                </div>
-            ))}
-        </div>
-
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No songs found.
+      </div>
     );
-}
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song) => (
+        // <div key={song.id}
+        //     className="flex items-center gap-x-4 w-full">
+        //     <div className="flex-1">
+        //         <MediaItem onClick={() => { }} data={song} />
+        //     </div>
+        // </div>
+        <div key={song.id} className="flex items-center gap-x-4 w-full">
+          <div className="flex-1">
+            <MediaItem onClick={() => {}} key={song.id} data={song} />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default SearchContent;
